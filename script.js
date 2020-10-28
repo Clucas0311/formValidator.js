fe4631c0a975ed6e04d476b285d5c7bc8a7646ca
Clucas0311 <clucas0311@gmail.com>
2020-10-28 17:04:29 -0400

    Adding a email verification

--- script.js
+++ script.js
@@ -35,9 +35,12 @@ form.addEventListener('submit', function(e) {
 	}
 	if (email.value === '') {
 		showError(email, 'Email is required');
+	} else if (!isValidEmail(email.value)) {
+		showError(email, 'Email is not valid');
 	} else {
 		showSuccess(email);
 	}
+
 	if (password.value === '') {
 		showError(password, 'Password is required');
 	} else {
