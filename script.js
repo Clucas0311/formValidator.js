9c682e7813d34203c7a8eddd97363769d3f76197
Clucas0311 <clucas0311@gmail.com>
2020-10-28 14:34:42 -0400

    Grabbing DOM Elements

---
+++ script.js
@@ -0,0 +1,6 @@
+// Grab the dom elements that we need..
+const form = document.querySelector('#form');
+const username = document.querySelector('#username');
+const email = document.querySelector('#email');
+const password = document.querySelector('#password');
+const password2 = document.querySelector('#password2');
