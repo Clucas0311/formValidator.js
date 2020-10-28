e963d49f92fd3a1abb361c68dd8eaf92cfcb9fd8
Clucas0311 <clucas0311@gmail.com>
2020-10-28 16:52:10 -0400

    Add error and success color bars

--- script.js
+++ script.js
@@ -4,3 +4,48 @@ const username = document.querySelector('#username');
 const email = document.querySelector('#email');
 const password = document.querySelector('#password');
 const password2 = document.querySelector('#password2');
+
+// Show input Error messages
+function showError(input, message) {
+	const formControl = input.parentElement;
+	formControl.className = 'form-control error';
+	const small = formControl.querySelector('small');
+	small.innerText = message;
+}
+
+// Show success outline
+function showSuccess(input) {
+	const formControl = input.parentElement;
+	formControl.className = 'form-control success';
+}
+// Check email is valid
+function isValidEmail(email) {
+	const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	return re.test(String(email).toLowerCase());
+}
+
+// Event listeners
+form.addEventListener('submit', function(e) {
+	e.preventDefault();
+
+	if (username.value === '') {
+		showError(username, 'Username is required');
+	} else {
+		showSuccess(username);
+	}
+	if (email.value === '') {
+		showError(email, 'Email is required');
+	} else {
+		showSuccess(email);
+	}
+	if (password.value === '') {
+		showError(password, 'Password is required');
+	} else {
+		showSuccess(password);
+	}
+	if (password2.value === '') {
+		showError(password2, 'Password2 is required');
+	} else {
+		showSuccess(password2);
+	}
+});
